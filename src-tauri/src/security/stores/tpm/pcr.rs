6f60c1b6b819c2f2dd85a8a1faeb0c876bf505b6
Paweł <134298056+FixeQD@users.noreply.github.com>
2026-01-20 23:28:42 +0100

    Modify PCR slots for boot configuration
    Reduced the number of PCR slots for boot from 8 to 1 and commented on the purpose of each slot.
    
    Signed-off-by: Paweł <134298056+FixeQD@users.noreply.github.com>

--- src-tauri/src/security/stores/tpm/pcr.rs
+++ src-tauri/src/security/stores/tpm/pcr.rs
@@ -1,15 +1,15 @@
 use tss_esapi::interface_types::algorithm::HashingAlgorithm;
 use tss_esapi::structures::{PcrSelectionListBuilder, PcrSlot};
 
-pub const PCR_SLOTS_FOR_BOOT: [PcrSlot; 8] = [
-    PcrSlot::Slot0,
-    PcrSlot::Slot1,
-    PcrSlot::Slot2,
-    PcrSlot::Slot3,
-    PcrSlot::Slot4,
-    PcrSlot::Slot5,
-    PcrSlot::Slot6,
-    PcrSlot::Slot7,
+pub const PCR_SLOTS_FOR_BOOT: [PcrSlot; 1] = [
+    // PcrSlot::Slot0, //-> Core BIOS/UEFI firmware
+    // PcrSlot::Slot1, //-> Platform configuration
+    // PcrSlot::Slot2, //-> UEFI drivers (e.g. GPU)
+    // PcrSlot::Slot3, //-> Extension config
+    // PcrSlot::Slot4, //-> Bootloader code
+    // PcrSlot::Slot5, //-> Partition table
+    // PcrSlot::Slot6, //-> State change events
+    PcrSlot::Slot7,    //-> Secure Boot state
 ];
 
 pub fn create_pcr_selection_for_boot_state(
