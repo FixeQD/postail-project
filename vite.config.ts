a90fcb3a24c8ed8010046a332d150fa206690901
Paweł Sobczak <github@fixeq.qzz.io>
2026-03-05 18:48:50 +0100

    optimize(build): switch minifier to esbuild for more aggresive deadcode cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,15 @@ import path from 'path'
 
 const host = process.env.TAURI_DEV_HOST
 
-// https://vite.dev/config/
 export default defineConfig(
 	(): UserConfig => ({
 		plugins: [react(), tailwindcss()],
+		esbuild: {
+			drop: ['console', 'debugger'],
+			legalComments: 'none',
+		},
 
-		// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-		//
-		// 1. prevent Vite from obscuring rust errors
 		clearScreen: false,
-		// 2. tauri expects a fixed port, fail if that port is not available
 		server: {
 			port: 1420,
 			strictPort: true,
@@ -27,7 +26,6 @@ export default defineConfig(
 					}
 				: undefined,
 			watch: {
-				// 3. tell Vite to ignore watching `src-tauri`
 				ignored: ['**/src-tauri/**'],
 			},
 		},
@@ -37,15 +35,14 @@ export default defineConfig(
 			},
 		},
 		build: {
-			minify: 'terser',
-			terserOptions: {
-				compress: {
-					drop_console: true,
-					drop_debugger: true,
-				},
-			},
+			minify: 'esbuild',
 			chunkSizeWarningLimit: 4000,
 			rollupOptions: {
+				treeshake: {
+					moduleSideEffects: false,
+					propertyReadSideEffects: false,
+					tryCatchDeoptimization: false,
+				},
 				output: {
 					manualChunks: {
 						monaco: [
